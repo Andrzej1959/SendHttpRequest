cdea6f66b7aeadb22fda7b6616565e39a51f63c3
luksza <luksza@am.gdynia.pl>
2018-12-10 22:52:00 +0100

    Poprawki 10.12.2018

--- SendHttpRequest/Form1.cs
+++ SendHttpRequest/Form1.cs
@@ -69,51 +69,61 @@ namespace SendHttpRequest
 
         private async void button1_ClickAsync(object sender, EventArgs e)
         {
-            HttpClient clientRest = new HttpClient();
+            HttpClient clientHttp = new HttpClient();
             HttpRequestMessage requestMessage = new HttpRequestMessage();
 
-            if (radioButton1.Checked) requestMessage.Method = HttpMethod.Get;
-            if (radioButton2.Checked) requestMessage.Method = HttpMethod.Post;
-            if (radioButton3.Checked) requestMessage.Method = HttpMethod.Put;
-            if (radioButton4.Checked) requestMessage.Method = HttpMethod.Delete;
-            //requestMessage.Method = HttpMethod.Head;
-
-            HttpContent content = new StringContent(textBoxContent.Text);
-            //
-            if(radioButtonJSON.Checked) content.Headers.ContentType.MediaType = "application/json";
-
-            requestMessage.Properties.Add("okokokokk", "qqqqqqqqqqqqqqqqqq");
-            requestMessage.Headers.Add("User-Agent", "programHTTPclient");
-
-            string url = textBoxUri.Text;
-
-            requestMessage.RequestUri = new Uri(url);
-            if (requestMessage.Method != HttpMethod.Get)
-                requestMessage.Content = content;
-
-            var response = await clientRest.SendAsync(requestMessage);
-            labelResponse.Text = await response.Content.ReadAsStringAsync();
-
-            var heders = response.Headers;
-
-            label1.Text = "-------->Response Heders:\n";
-            foreach (var heder in heders)
+            try
             {
-                label1.Text += heder.Key + " = ";
-                foreach (var val in heder.Value)
-                    label1.Text += val + "  ";
-                label1.Text += "\n";
+                if (radioButton1.Checked) requestMessage.Method = HttpMethod.Get;
+                if (radioButton2.Checked) requestMessage.Method = HttpMethod.Post;
+                if (radioButton3.Checked) requestMessage.Method = HttpMethod.Put;
+                if (radioButton4.Checked) requestMessage.Method = HttpMethod.Delete;
+                //requestMessage.Method = HttpMethod.Head;
+
+                HttpContent content = new StringContent(textBoxContent.Text);
+                //
+                if (radioButtonJSON.Checked) content.Headers.ContentType.MediaType = "application/json";
+
+                requestMessage.Properties.Add("okokokokk", "qqqqqqqqqqqqqqqqqq");
+                requestMessage.Headers.Add("User-Agent", "programHTTPclient");
+
+                string url = textBoxUri.Text;
+
+                requestMessage.RequestUri = new Uri(url);
+                if (requestMessage.Method != HttpMethod.Get)
+                    requestMessage.Content = content;
+
+                var response = await clientHttp.SendAsync(requestMessage);
+                   
+                labelResponse.Text = await response.Content.ReadAsStringAsync();
+
+                var heders = response.Headers;
+
+                label1.Text = "-------->Response Heders:\n";
+                foreach (var heder in heders)
+                {
+                    label1.Text += heder.Key + " = ";
+                    foreach (var val in heder.Value)
+                        label1.Text += val + "  ";
+                    label1.Text += "\n";
+                }
+
+                label1.Text += "\n-------->Content Heders:\n";
+
+                foreach (var heder in response.Content.Headers)
+                {
+                    label1.Text += heder.Key + " = ";
+                    foreach (var val in heder.Value)
+                        label1.Text += val + "  ";
+                    label1.Text += "\n";
+                }
             }
 
-            label1.Text += "\n-------->Content Heders:\n";
-
-            foreach (var heder in response.Content.Headers)
+            catch (Exception exep)
             {
-                label1.Text += heder.Key + " = ";
-                foreach (var val in heder.Value)
-                    label1.Text += val + "  ";
-                label1.Text += "\n";
-            }     
+                labelResponse.Text = "Wystapił błąd: " + exep.Message;
+            }
+
         }
     }
 }
